e90774ff1a34e5dc6742997aef49309d14d617be
Timofey <crysistima@mail.ru>
2020-10-12 17:13:40 +0300

    saving changes

---
+++ src/components/Profile/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Post.module.css'
+
+const Post = () => {
+  return (
+    <div className={s.item}>
+
+      <img src='https://vse-footbolki.ru/image/catalog/vsm/0/1/1334/1334537/previews/people_1_holst_square_full_front_white_700.jpg' />
+      Post1
+    </div>
+  )
+}
+
+export default Post;
---
+++ src/components/Profile/Posts/Posts.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Posts.module.css'
+
+const Posts = () => {
+    return(
+        <a>fsgfsd</a>
+    )
+}
+
+export default Posts;
