edc01c2995659bcd8f12ad1d54a678887d1eacbc
Timofey <crysistima@mail.ru>
2020-10-19 16:31:08 +0300

    Немного зарефакторил css и добавил react router

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import Posts from './Posts/Posts'
 const Profile = () => {
     return(
       <div>
-        <div className={s.content}>
+        <div>
         <img src='https://vse-footbolki.ru/image/catalog/vsm/0/1/1334/1334537/previews/people_1_holst_square_full_front_white_700.jpg' />
       </div>
       <Posts/>
