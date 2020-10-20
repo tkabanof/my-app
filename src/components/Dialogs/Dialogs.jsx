6bd3fdda652ebef88fbeca8ea1294074373f9b3d
Timofey <crysistima@mail.ru>
2020-10-20 15:29:35 +0300

    Message разбил на компоненты. Рефактор. Добавил NavLink в Message.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    Vano
-                </div>
-                <div className={s.dialog}>
-                    Petro
-                </div>
-                <div className={s.dialog}>
-                    Mahno
-                </div>
+                <DialogItem name="Dim" id="1"/>
+                <DialogItem name="Tim" id="2"/>
+                <DialogItem name="Sanya" id="3"/>
+                <DialogItem name="Tanya" id="4"/>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>Hi u</div>
-                <div className={s.message}>stupid</div>
+                <Message message="Yoba"/>
+                <Message message="Text1"/>
+                <Message message="Txet2"/>
+                <Message message="Hollo Friends"/>
             </div>
         </div>
     )
