09b6bf458ada41927a85df6f2a0cc276cdd59df4
Timofey <crysistima@mail.ru>
2020-10-20 10:23:20 +0300

    Заглушки для страниц Новости, музыка и настройки

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
@@ -13,10 +16,13 @@ const App = () => {
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
-                {/*<Profile />*/}
+
                 <div className='app-wrapper-content'>
                     <Route path="/dialogs" component={Dialogs}/>
                     <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/Music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
 
                 </div>
             </div>
