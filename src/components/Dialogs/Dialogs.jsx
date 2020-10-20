98a6225df0cd6719443e1fef35e9e9e312ec5c67
Timofey <crysistima@mail.ru>
2020-10-20 12:45:51 +0300

    Добавил макет Messages.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Vano
+                </div>
+                <div className={s.dialog}>
+                    Petro
+                </div>
+                <div className={s.dialog}>
+                    Mahno
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hi u</div>
+                <div className={s.message}>stupid</div>
+            </div>
         </div>
     )
 }
