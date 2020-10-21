6af9006c9e953e688f1b0ae43c63af1eb6f9f849
Timofey <crysistima@mail.ru>
2020-10-21 14:20:09 +0300

    Небольшой рефактор

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import s from './Post.module.css'
 
 const Post = (props) => {
-  return (
-    <div className={s.item}>
-      <img src = 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg' />
-      {props.message}
-      <div className = {s.heart}></div>
-  <span>{props.likes}</span>
-    </div>
-  )
+    return (
+        <div className={s.item}>
+            <div>
+                <img src='https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg'/>
+            </div>
+            <div className={s.message}>
+                {props.message}
+            </div>
+            <div>
+                <div className={s.heart}></div>
+                <span>{props.likes}</span>
+            </div>
+        </div>
+    )
 }
 
 export default Post;
