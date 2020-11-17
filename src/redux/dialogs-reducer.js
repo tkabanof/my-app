ef5e114acf50814857264d38769bb0eb2389bccd
Timofey <crysistima@mail.ru>
2020-11-17 12:48:52 +0300

    исправил Страницу messges

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,8 +1,19 @@
+let initialStore = {
+    dialogs: [
+        {name: "Dima", id: '1'},
+        {name: "Tim", id: '2'},
+        {name: "Tanya", id: '3'}
+    ],
+    messages: [
+        {message: 'Yoba', id: '1'},
+        {message: 'Text1', id: '2'},
+        {message: 'Text2', id: '3'},
+    ]
+}
 
-
-const dialogsReducer = (state, action) => {
-
-
+const dialogsReducer = (state = initialStore, action) => {
 
     return state;
 }
+
+export default dialogsReducer;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,8 +1,10 @@
 import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
 
 let reducers = combineReducers({
-    profile: profileReducer
+    profile: profileReducer,
+    dialogsState: dialogsReducer
 });
 
 let store = createStore(reducers);
