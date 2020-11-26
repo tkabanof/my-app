4c18d3e27b4547872c6e37cdc20f362c365814a3
Timofey <crysistima@mail.ru>
2020-11-26 12:51:24 +0300

    Исправил импорт axios аконец то получилось подгрузить пользователей с сервера

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,59 +1,33 @@
 import React from 'react';
 import Friend from "./FriendCard/Friend";
-import {NodePath as axios} from "@babel/traverse";
+import * as axios from "axios"
+import user_photo from "../../assets/images/userpic.png"
 
 
 const Friends = (props) => {
 
-if (props.friends.length === 0) {
-    props.setFriends(
-        [
-            {
-                userid: 1,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Dima',
-                birthday: '2000-01-01',
-                followed: true
-            }
-            ,
-            {
-                userid: 2,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Tanya',
-                birthday: '2010-12-31',
-                followed: false
-            }
-            ,
-            {
-                userid: 3,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Tim',
-                birthday: '2011-12-31',
-                followed: true
-            }
-            ,
-            {
-                userid: 4,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Dan',
-                birthday: '2001-02-31',
-                followed: true
-            }
-        ]
-    );
-}
-
-    let friendItem = props.friends.friendslist.map(m => <Friend key={m.userid}
-                                                                userid={m.userid}
-                                                                name={m.name}
-                                                                birthday={m.birthday}
-                                                                avaLink={m.avaLink}
-                                                                followed={m.followed}
-                                                                follow={props.follow}
-                                                                unfollow={props.unfollow}/>)
+    let getUsers = () => {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setFriends(response.data.items)
+            });
+    }
+    let friendItem = props.friends.items.map(m => <Friend key={m.userid}
+                                                          userid={m.id}
+                                                          name={m.name}
+                                                          birthday={"BIRTHDAY"}
+                                                          avaLink={user_photo}
+                                                          followed={m.followed}
+                                                          follow={props.follow}
+                                                          unfollow={props.unfollow}/>)
     return (
         <div>
-            {friendItem}
+            <div>
+                <button onClick={getUsers}>Get Users</button>
+            </div>
+            <div>
+                {friendItem}
+            </div>
         </div>
     )
 }
--- src/redux/friend-reducer.js
+++ src/redux/friend-reducer.js
@@ -2,49 +2,53 @@ const FOLLOW = 'FOLLOWACTION'
 const UNFOLLOW = 'UNFOLLOW ACTION'
 const SET_FRIENDS = 'SET_FRIENDS'
 
+
 let initialStore =
-    {
-        friendslist: [
-            {
-                userid: 1,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Dima',
-                birthday: '2000-01-01',
-                followed: true
-            }
-            ,
-            {
-                userid: 2,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Tanya',
-                birthday: '2010-12-31',
-                followed: false
-            }
-            ,
-            {
-                userid: 3,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Tim',
-                birthday: '2011-12-31',
-                followed: true
-            }
-            ,
-            {
-                userid: 4,
-                avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-                name: 'Dan',
-                birthday: '2001-02-31',
-                followed: true
-            }
-        ]
+    { items : []
+        // items: [
+        //     {
+        //         userid: 1,
+        //         avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+        //         name: 'Dima',
+        //         birthday: '2000-01-01',
+        //         followed: true
+        //     }
+        //     ,
+        //     {
+        //         userid: 2,
+        //         avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+        //         name: 'Tanya',
+        //         birthday: '2010-12-31',
+        //         followed: false
+        //     }
+        //     ,
+        //     {
+        //         userid: 3,
+        //         avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+        //         name: 'Tim',
+        //         birthday: '2011-12-31',
+        //         followed: true
+        //     }
+        //     ,
+        //     {
+        //         userid: 4,
+        //         avaLink: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+        //         name: 'Dan',
+        //         birthday: '2001-02-31',
+        //         followed: true
+        //     }
+        // ]
     }
 
 const FriendReducer = (state = initialStore, action) => {
+    console.log(state);
+    console.log(action);
+
     switch (action.type) {
         case FOLLOW: {
             return {
                 ...state,
-                friendslist: state.friendslist.map(u => {
+                items: state.items.map(u => {
                     if (u.userid === action.userid) {
                         return {
                             ...u,
@@ -59,7 +63,7 @@ const FriendReducer = (state = initialStore, action) => {
         case UNFOLLOW: {
             return {
                 ...state,
-                friendslist: state.friendslist.map(u => {
+                items: state.items.map(u => {
                     if (u.userid === action.userid) {
                         return {
                             ...u,
@@ -71,7 +75,7 @@ const FriendReducer = (state = initialStore, action) => {
             }
         }
         case SET_FRIENDS: {
-            return {...state, friendslist: [state.friendslist, ...action.friendslist]}
+            return {...state, items: [state.items, ...action.friends]}
         }
         default: {
             return state;
@@ -93,10 +97,10 @@ export const unFollowAC = (userid) => {
     }
 }
 
-export const setFriendsAC = (friendslist) => {
+export const setFriendsAC = (friends) => {
     return {
         type: SET_FRIENDS,
-        friendslist: friendslist
+        friends: friends
     }
 }
 
