1e039704c2eaadfba49ff0a540685ea3320d57b8
Timofey <crysistima@mail.ru>
2020-12-18 15:50:13 +0300

    Валидация для логина

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,42 @@
 import {Field, reduxForm} from "redux-form";
 import {userAPI} from "../../api/api";
+import {maxLength15, minLength2, required} from "../../utils/validators";
+
+const renderField = ({
+                                input,
+                                label,
+                                type,
+                                meta: { touched, error, warning }
+                            }) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} placeholder={label} type={type} />
+            {touched &&
+            ((error && <span>{error}</span>) ||
+                (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+);
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+                <Field
+                       name={"login"}
+                       label = {"Имя пользователя"}
+                       component={renderField}
+                       validate={[required, maxLength15, minLength2]}
+                />
             </div>
             <div>
-                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+                <Field
+                       name={"password"}
+                       component={renderField}
+                       label = {"Пароль"}
+                       validate={[required, maxLength15, minLength2]}
+                />
             </div>
             <div>
                 <Field component={"input"} type={"checkbox"} name={"rememberMe"}/>remeber me
