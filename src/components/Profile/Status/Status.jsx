6a5e31bdfbd8469c588c6d7d22cb5dd8397f60cc
Timofey <crysistima@mail.ru>
2021-02-02 17:06:58 +0300

    Убрал логи

--- src/components/Profile/Status/Status.jsx
+++ src/components/Profile/Status/Status.jsx
@@ -3,16 +3,11 @@ import {useState, useEffect} from 'react';
 
 const Status = (props) => {
 
-    console.log('props ' + props.status);
-
     const [editMode, setEditMode] = useState(false);
     const [textStatus, setTextStatus] = useState(props.status);
 
-    console.log('useState text status ' + textStatus);
-
     useEffect(() => {
         setTextStatus(props.status);
-        console.log('эффект');
     }, [props.status]);
 
     const changeStatusText = (e) => {
@@ -28,12 +23,8 @@ const Status = (props) => {
     function disableEdit() {
         setEditMode(false);
         if (textStatus !== props.status) {
-            console.log('Изменение статуса. Новый статус ' + textStatus);
             props.setMyStatus(textStatus);
-        } else {
-            console.log('статус отсался прежним');
         }
-
     }
 
     return (
