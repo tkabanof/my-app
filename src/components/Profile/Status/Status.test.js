ae2065b25f533775dbe2b2989fdf43646fd442de
Timofey <crysistima@mail.ru>
2021-03-04 12:16:50 +0300

    Шаблон теста работает

--- src/components/Profile/Status/Status.test.js
+++ src/components/Profile/Status/Status.test.js
@@ -1,14 +1,29 @@
-import {create} from "react-test-renderer";
+import {create, act} from "react-test-renderer";
 import Status from "./Status";
+import {unmountComponentAtNode} from "react-dom";
 
+
+let container = null;
+beforeEach(() => {
+    // подготавливаем DOM-элемент, куда будем рендерить
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // подчищаем после завершения
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
 
 describe("Status component", () => {
     test("status from props should be in the state", () => {
-        const component = create(<Status status="blblstatus"/>);
-        const instance = component.getInstance();
-        expect(instance.state.status).toBe("blblstatus");
-
-
+        let component;
+        act(() => {
+            component = create(<Status status="blblstatus"/>);
+        });
+        expect(component.toJSON()).toMatchSnapshot();
     });
 });
 
