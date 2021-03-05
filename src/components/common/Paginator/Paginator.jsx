098a3f8adb55dde62b58987624785609e5aff744
Timofey <crysistima@mail.ru>
2021-03-05 11:08:29 +0300

    Meterial UI пагинатор

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,22 +1,31 @@
 import s from "./Paginator.module.css"
+import Pagination from '@material-ui/lab/Pagination';
+import { makeStyles } from '@material-ui/core/styles';
+import {useState} from "react";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > * + *': {
+            marginTop: theme.spacing(1),
+        },
+    },
+}));
 
 const Paginator = (props) => {
 
-    let pageCount = Math.ceil(props.totalCount / props.pageSize);
-    let pages = [];
-    for (let i = 1; i <= pageCount; i++) {
-        pages.push(i);
-    }
-    return(
-        <div>
-            {pages.map(p => {
-                return <span className={props.currentPage === p ? s.selectedPage : null}
-                             onClick={(e) => {
-                                 props.onPageChanged(p)
-                             }}>{p} </span>
-            })}
+
+    const classes = useStyles();
+    const [page, setPage] = useState(1);
+    const handleChange = (event, value) => {
+        setPage(value);
+        props.onPageChanged(value);
+    };
+
+    return (
+        <div className={classes.root}>
+        <Pagination count={Math.floor(props.totalCount/20 + 1)} page={page} onChange={handleChange} />
         </div>
-    )
+    );
 }
 
 export default Paginator;
