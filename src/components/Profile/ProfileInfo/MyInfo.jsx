b5b960cc1f1d5e1b40e738f7b831e164e0f777c9
Timofey <crysistima@mail.ru>
2021-03-17 14:28:29 +0300

    Небольшой рефатор

--- src/components/Profile/ProfileInfo/MyInfo.jsx
+++ src/components/Profile/ProfileInfo/MyInfo.jsx
@@ -2,33 +2,30 @@ import s from "./ProfileInfo.module.css";
 import {SocialIcon} from 'react-social-icons';
 
 const Social = (props) => {
-    return (<div>
-        <SocialIcon url={props.link} network={props.network}/>
-        <a> </a>
-        {props.link}
-    </div>)
+    return (<SocialIcon url={props.link} network={props.network}/>)
 }
 
 const MyInfo = (props) => {
 
     let p = props.p;
 
-    return (
-        <div className={s.description}>
-
-            <p>{p.aboutMe}</p>
-            <Social link={p.contacts.facebook} network={"facebook"}/>
-            <Social link={p.contacts.vk} network={"vk"}/>
-            <Social link={p.contacts.twitter} network={"twitter"}/>
-            <Social link={p.contacts.instagram} network={"instagram"}/>
-            <Social link={p.contacts.youtube} network={"youtube"}/>
-            <Social link={p.contacts.github} network={"github"}/>
-            <Social link={p.contacts.mainLink} network="mailto"/>
-            <div>
-                <p>{p.lookingForAJob ? "Ищу работу" : "Есть работа"}</p>
-                <p>{p.lookingForAJob ? p.lookingForAJobDescription : ""}</p>
+    return (<div>
+            <div className={s.description}>
+                <div>
+                    <p>{p.aboutMe}</p>
+                    <p>{p.lookingForAJob ? "Ищу работу" : "Есть работа"}</p>
+                    <p>{p.lookingForAJob ? p.lookingForAJobDescription : ""}</p>
+                </div>
             </div>
-
+            <div className={s.social}>
+                <Social link={p.contacts.facebook} network={"facebook"}/>
+                <Social link={p.contacts.vk} network={"vk"}/>
+                <Social link={p.contacts.twitter} network={"twitter"}/>
+                <Social link={p.contacts.instagram} network={"instagram"}/>
+                <Social link={p.contacts.youtube} network={"youtube"}/>
+                <Social link={p.contacts.github} network={"github"}/>
+                <Social link={p.contacts.mainLink} network="mailto"/>
+            </div>
         </div>
     )
 }
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -50,7 +50,6 @@ const ProfileInfo = (props) => {
             {editMode &&
             <MyInfoForm updateMyInfo={props.updateMyInfo}/>
             }
-
         </div>
     )
 }
